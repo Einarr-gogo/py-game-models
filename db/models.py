b2746b955e4a7683b70d16dbdb388d6fa47e5eb2
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-06-03 21:23:06 +0200

    Implement

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -12,7 +13,8 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
     def __str__(self) -> str:
         return self.name
@@ -31,7 +33,8 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
+                              null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
